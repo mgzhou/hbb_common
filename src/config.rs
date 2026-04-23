74d7ee3ee772cb52626e1d78108036dd18d5744e
mgzhou <1398551695@qq.com>
2026-04-23 08:33:26 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -156,8 +156,9 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["175.178.225.161"];
+pub const API_SERVER: &str = "http://175.178.225.161:21114";
+pub const RS_PUB_KEY: &str = "07WFmQz+AZRdDQwT75kN1GoBNazjb7wryBb3RkWMyzg=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
